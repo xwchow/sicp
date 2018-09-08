2fc235bae81c64255c033163e9002c1100bca5b2
Xin Wei Chow <chowxinwei@gmail.com>
2018-09-08 18:53:57 +1000

    Finished up to Exercise 1.28.

--- Ch1/Chapter1.scm
+++ Ch1/Chapter1.scm
@@ -169,3 +169,161 @@
 
 (fib 200)
 
+;; Exercise 1.20
+
+;; Applicative order
+(gcd 206 40)
+(gcd 40 6) ; (1)
+(gcd 6 4) ; (2)
+(gcd 4 2) ; (3)
+(gcd 2 0) ; (4)
+
+;; Normal order
+(gcd 206 40)
+;; if (= 40 0) ... else
+(gcd 40 (remainder 206 40))
+;; (if (= (remainder 206 40) 0)) else ... (1)
+(gcd (remainder 206 40) (remainder 40 (remainder 206 40)))
+;; if (= (remainder 40 (remainder 206 40)) 0) else ... (3)
+(gcd (remainder 40 (remainder 206 40))
+     (remainder (remainder 206 40)
+                (remainder 40 (remainder 206 40))))
+;; (if (remainder (remainder 206 40)
+;;                (remainder 40 (remainder 206 40)))
+;;     0) else ... (7)
+(gcd (remainder (remainder 206 40)
+                (remainder 40 (remainder 206 40)))
+     (remainder (remainder 40 (remainder 206 40))
+                (remainder (remainder 206 40)
+                           (remainder 40 (remainder 206 40)))))
+;; (if (remainder (remainder 40 (remainder 206 40))
+;;                 (remainder (remainder 206 40)
+;;                            (remainder 40 (remainder 206 40))))
+;;     0) (14)
+(remainder (remainder 206 40)
+           (remainder 40 (remainder 206 40))) ;; (18)
+
+;; Exercise 1.21
+(define (smallest-divisor n)
+  (define (square x) (* x x))
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (find-divisor n 2))
+
+(define (prime? n)
+  (if (< n 2)
+      #f
+      (= n (smallest-divisor n))))
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
+
+;; Exercise 1.22
+;; Displays time in milliseconds.
+(define (runtime)
+  (define now (gettimeofday))
+  (+ (car now) (/ (cdr now) 1000.)))
+
+(define (timed-prime-test n)
+  (define start-time (runtime))
+  (define (display-time)
+    (display n)
+    (display " is prime.")
+    (display " Time taken: ")
+    (display (- (runtime) 
+                start-time))
+    (display " ms")
+    (newline))
+  (cond ((prime? n) (display-time) #t)
+        (else #f)))
+
+(timed-prime-test 1000000007)
+
+(define (find-primes n k)
+  (define (find-primes-count n count)
+    (cond ((= k count) 0)
+          ((timed-prime-test n) (find-primes-count (+ n 2) (+ count 1)))
+          (else (find-primes-count (+ n 2) count))))
+  (if (= 0 (remainder n 2))
+      (find-primes-count (+ n 1) 0)
+      (find-primes-count n 0)))
+
+(find-primes 1000000000 3) ;; 1.5 ms
+(find-primes 10000000000 3) ;; 4 ms
+(find-primes 100000000000 3) ;; 12 ms
+
+;; Exercise 1.23
+(define (smallest-divisor n)
+  (define (square x) (* x x))
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (define (next x)
+    (if (= x 2)
+        3
+        (+ x 2)))
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (next test-divisor)))))
+  (find-divisor n 2))
+
+(find-primes 1000000000 3) ;; 1 ms
+(find-primes 10000000000 3) ;; 2 ms
+(find-primes 100000000000 3) ;; 6 ms
+
+;; Exercise 1.27
+(define (expmod a n m)
+  (define (square x) (* x x))
+  (define (even? x) (= (remainder x 2) 0))
+  (cond ((= n 0) 1)
+        ((even? n) (remainder (square (expmod a (/ n 2) m))
+                              m))
+        (else (remainder (* a (expmod a (- n 1) m)) m))))
+
+(define (fermat-theorem-test n)
+  (define (iter a)
+    (cond ((= a n) #t)
+          ((not (= (expmod a n n) a)) #f)
+          (else (iter (+ a 1)))))
+  (iter 1))
+
+(fermat-theorem-test 561)
+(fermat-theorem-test 1105)
+(fermat-theorem-test 1729)
+(fermat-theorem-test 6601)
+
+;; Exercise 1.28
+(define (expmod-mr a n m)
+  (define (square x)
+    (define result (remainder (* x x) m))
+    (if (or
+         (= x 1)
+         (= x (- m 1))
+         (not (= result 1)))
+        result
+        0))
+  (define (even? x) (= (remainder x 2) 0))
+  (cond ((= n 0) 1)
+        ((even? n) (square (expmod-mr a (/ n 2) m)))
+        (else (remainder (* a (expmod-mr a (- n 1) m)) m))))
+
+(define (miller-rabin n)
+  (define (try-it a)
+    (= 1 (expmod-mr a (- n 1) n)))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) #t)
+        ((miller-rabin n) (fast-prime? n (- times 1)))
+        (else #f)))
+
+(fast-prime? 561 20)
+(fast-prime? 1105 20)
+(fast-prime? 1000000007 20)
+(fast-prime? 1024 20)
+  
